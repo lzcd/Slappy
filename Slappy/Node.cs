3d45baea57f7983e70542449a3de1e51a8a38060
lzcd <lzcd@hotmail.com>
2011-05-20 15:48:22 +1000

    Added solution

---
+++ Slappy/Node.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Dynamic;
+
+namespace Slappy
+{
+    public class Node : DynamicObject
+    {
+        private Dictionary<string, object> valueByPath;
+
+        public Node()
+        {
+            valueByPath = new Dictionary<string, object>();
+        }
+
+        private Node parent;
+        private string name;
+        
+        protected Node(Node parent, string name)
+        {
+            this.parent = parent;
+            this.name = name;
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            if (!TryGetValue(binder.Name, out result))
+            {
+                result = new Node(this, binder.Name);
+            }
+            
+            return true;
+        }
+
+        protected bool TryGetValue(string path, out Object value)
+        {
+            if (parent == null)
+            {
+                if (valueByPath.TryGetValue(path, out value))
+                {
+                    return true;
+                }
+
+                return false;
+            }
+            else
+            {
+                return parent.TryGetValue(name + "/" + path, out value);  
+            }
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            return TrySetValue(binder.Name, value);
+        }
+
+        protected bool TrySetValue(string path, Object value)
+        {
+            if (parent == null)
+            {
+                valueByPath[path] = value;
+            }
+            else
+            {
+                return parent.TrySetValue(name + "/" + path, value);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Slappy;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            dynamic one = new Node();
+            one.People.Gerald.Age = 34;
+            Assert.AreEqual(34, one.People.Gerald.Age);
+        }
+    }
+}
